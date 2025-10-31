b4b2dc7330e3bf5ee845ad5a9dbd43365d0fe287
WR-19 <wasimrahman410@gmail.com>
2025-10-31 00:41:31 +0000

    feat: implement VPC module with networking components
    - Create VPC with DNS support enabled
    - Deploy public and private subnets across AZs
    - Configure internet gateway for public subnets
    - Set up route tables and associations
    - Add outputs for other modules to use

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,25 @@
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnets CIDR blocks"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+  default     = {}
+}
