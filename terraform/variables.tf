8881f746db70b20275c0928fe6db7e1f9e439ad8
WR-19 <wasimrahman410@gmail.com>
2025-10-31 01:51:10 +0000

    fix: add missing azs variable definition

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,7 +22,7 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "availability_zones" {
+variable "azs" {
   description = "Availability zones"
   type        = list(string)
   default     = ["us-east-1a", "us-east-1b"]
@@ -32,22 +32,4 @@ variable "public_subnets" {
   description = "Public subnets CIDR blocks"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnets" {
-  description = "Private subnets CIDR blocks"
-  type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
-}
-
-variable "database_name" {
-  description = "Database name"
-  type        = string
-  default     = "appdb"
-}
-
-variable "database_user" {
-  description = "Database username"
-  type        = string
-  default     = "appuser"
 }
